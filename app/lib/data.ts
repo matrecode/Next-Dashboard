0f540002933e631cf249a8ea5acf1cf3cd90d7b8
matrecode <akshaymatrecode@gmail.com>
2024-03-07 16:07:47 +0530

    Learnt and implemented data fetching and server component

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -64,6 +64,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
 
+        //  parallel data fetching
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
