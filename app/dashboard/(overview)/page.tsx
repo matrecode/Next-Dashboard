ed9061b6b8de6cb51c2bb30a177b623e16671b94
matrecode <akshaymatrecode@gmail.com>
2024-03-09 14:48:48 +0530

    learnt static and dynamic rendering and streaming to imrove user experience, added suspense and loading state

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,16 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '../lib/data';
- 
+import { fetchCardData } from '../../lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+
 export default async function Page() {
 
-    const revenue = await fetchRevenue()
-    const latestInvoices = await fetchLatestInvoices()
-    const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData()
-    console.log(revenue)
+  // const revenue = await fetchRevenue()
+  // const latestInvoices = await fetchLatestInvoices()
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData()
+  // console.log(revenue)
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -26,8 +28,13 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        < Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton/>}>
+
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
